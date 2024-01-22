cb529ecca6a2f53ee1ffaf6b4879b0b4f5fe584e
Bluepenguin9939 <156149340+Bluepenguin9939@users.noreply.github.com>
2024-01-22 14:51:41 +0900

    Create HomeController2.java

---
+++ src/main/java/com/kh/minCinema/controller/HomeController2.java
@@ -0,0 +1,15 @@
+package com.kh.minCinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController2 {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "redirect:/main/jo_main";
+	}
+	
+}
