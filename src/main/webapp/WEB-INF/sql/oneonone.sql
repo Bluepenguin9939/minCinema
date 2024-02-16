ed1b567f5f20d25f2ec3aaac8cf649666ad42618
libera07 <jemu007@naver.com>
2024-02-16 12:15:50 +0900

    수우우우우정

--- src/main/webapp/WEB-INF/sql/oneonone.sql
+++ src/main/webapp/WEB-INF/sql/oneonone.sql
@@ -1,7 +1,7 @@
 CREATE TABLE MC_ONEONONE(
     rn NUMBER PRIMARY KEY, --시퀀스 적용할 번호 큰의미는 없음
     msg_id  VARCHAR2(50), --ID 기본키
-    sender VARCHAR2(50) CONSTRAINT FK_SENDER REFERENCES MC_MEMBER(MID), -- 멤버 ID 참조 
+    sender VARCHAR2(50) CONSTRAINT FK_SENDER REFERENCES MC_MEMBER(MID) ON DELETE CASCADE, --(2024/02/16 수정) 멤버 ID 참조 
     mtitle VARCHAR2(50) NOT NULL,
     message VARCHAR2(500), --문의사항
     remessage varchar2(500),
